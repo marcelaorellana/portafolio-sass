1fe5ed13e38393005bbd92958204497f2916e011
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-07-21 11:58:14 -0400

    navbar y slider

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	var scroll_start = 0;
+	var startchange = $('nav');
+	var offset = startchange.offset();
+	$('.slider').slider({
+    height: 600
+    });
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('nav').css('background-color', 'white');
+          $('.nav-wrapper ul li a').css('color', 'gray');
+       } else {
+          $('nav').css('background-color', 'transparent');
+          $('.nav-wrapper ul li a').css('color', 'white');
+       }
+   });
+});
