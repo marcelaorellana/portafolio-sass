4a5443276d0a3a7d8a30b09edd2266c40e762321
marcelaorellana <marcelapaz.1985@gmail.com>
2017-07-22 23:52:00 -0400

    maquetado completo

--- js/main.js
+++ js/main.js
@@ -11,9 +11,12 @@ $(document).ready(function(){
       if(scroll_start > offset.top) {
           $('nav').css('background-color', 'white');
           $('.nav-wrapper ul li a').css('color', 'gray');
+          $('nav .brand-logo').css('color', 'gray');
+          $('.nav-wrapper ul li a.btn').css('color', 'white');
        } else {
           $('nav').css('background-color', 'transparent');
           $('.nav-wrapper ul li a').css('color', 'white');
+          $('nav .brand-logo').css('color', 'white');
        }
    });
 });
